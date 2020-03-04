5917e4541c33040290de726b10d90e9fe3cb41de
ab0v3g4me <udzizz@gmail.com>
2020-03-04 11:53:18 +0100

    MenuState finished

--- src/states/menu.rs
+++ src/states/menu.rs
@@ -2,8 +2,7 @@ use amethyst::{
     ecs::WorldExt,
     State, StateData, Trans, TransEvent, StateEvent,
     assets::{Handle, ProgressCounter, AssetStorage, Loader},
-    renderer::SpriteSheet,
-    ui::{TtfFormat,FontAsset, UiButtonBuilder, Anchor, UiText},
+    ui::{TtfFormat,FontAsset, UiButtonBuilder, Anchor, UiText,UiEvent, UiEventType},
 };
 use crate::{
     cgd::GameExecData,
@@ -32,9 +31,30 @@ impl<'a, 'b> State<GameExecData<'a, 'b>, StateEvent> for MenuState {
         
         crate::utilities::initialize_camera(world);
         self.generator.generate_buttons(world, vec!["NEW GAME", "OPTIONS", "QUIT"]);
+        
     }
+    fn handle_event(&mut self, _: StateData<GameExecData>, event: StateEvent,) -> Trans<GameExecData<'a, 'b>, StateEvent> {
+        match event {
+            StateEvent::Ui(UiEvent {
+                event_type: UiEventType::Click,
+                target,
+            }) => {
+                println!("{:?}", self.generator.widgets.contains(&target));
+                Trans::None
+            },
+            _ => Trans::None,
+        }
+    }
+
     fn update(&mut self,data: StateData<GameExecData>) -> Trans<GameExecData<'a, 'b>, StateEvent> {
         data.data.update(&data.world, true);
         Trans::None
+    }
+
+    fn on_stop(&mut self,data: StateData<GameExecData>) {
+        let world = data.world;
+
+        world.delete_entities(&self.generator.widgets[..]);
+        self.generator.widgets.clear();
     }
 }
--- src/utilities/ui.rs
+++ src/utilities/ui.rs
@@ -3,33 +3,30 @@ use amethyst::{
         UiTransform, Anchor, UiButtonBuilder, FontAsset, UiText,
     },
     assets::{Handle},
-    input::StringBindings,
     ecs::{World, WorldExt, Entity},
     ecs::prelude::{Builder},
-    window::ScreenDimensions,
 };
+use std::collections::HashMap;
+
 /// Generates specified UI. 
 ///
 pub struct UiGenerator {
-    font_handle: Handle<FontAsset>,
     builder: UiButtonBuilder<u32, u32>,
-    widgets: Vec<Entity>,
+    pub widgets: Vec<Entity>,
 }
 impl UiGenerator {
-    const FONT_SIZE: f32 = 30f32;
-    const BUTTON_OFFSET: f32 = 30f32;
-
+    const FONT_SIZE: f32 = 50f32;
+    const BUTTON_INNER_OFFSET: f32 = 40f32;
+    
     pub fn new(font_handle: Handle<FontAsset>) -> Self {
         let widgets = Vec::new();
 
         let builder = UiButtonBuilder::new("DEFAULT BUTTON")
             .with_anchor(Anchor::Middle)
-            .with_font(font_handle.clone())
+            .with_font(font_handle)
             .with_font_size(Self::FONT_SIZE);
         
-
         Self {
-            font_handle,
             builder,
             widgets,
         }
@@ -40,58 +37,34 @@ impl UiGenerator {
 
         for (idx, text) in buttons.iter().enumerate() {
             let (x_size, y_size) = Self::acquire_size(text.len());
-            let (x, y) = Self::acquire_y_position(world, length as u32, idx as u32, (x_size, y_size));
+            let y = Self::acquire_y_position(world, length as u32, idx as u32);
 
-            let ui_text = UiText::new(
-                self.font_handle.clone(),
-                String::from(*text),
-                [0.0,0.0,0.0,1.0],
-                Self::FONT_SIZE,
-            );
-            let ui_position = UiTransform::new(
-                String::from(*text), 
-                Anchor::Middle, 
-                Anchor::Middle, 
-                x, 
-                y,
-                0f32, 
-                x_size, 
-                y_size,
-            );
-              
-            world.create_entity()
-                .with(ui_text)
-                .with(ui_position)
-                .build();
+            let (_, button) = self.builder.clone()
+                .with_position(0f32, y)
+                .with_size(x_size, y_size)
+                .with_text(String::from(*text))
+                .with_text_color([1f32,1f32,1f32,0.5f32])
+                .with_hover_text_color([1f32,1f32,1f32,1f32])
+                .build_from_world(world);
+
+            self.widgets.push(button.image_entity);  
         }
     }
 
-    fn acquire_y_position(world: &mut World, num_of_buttons: u32,idx: u32, (size_width, size_height): (f32, f32)) -> (f32, f32) {
-        let (mut x, mut y) = (0f32, 0f32);
+    fn acquire_y_position(world: &mut World, num_of_buttons: u32,idx: u32) -> f32 {
+        let starting_y_offset = num_of_buttons as f32 * Self::BUTTON_INNER_OFFSET;
+        let y_step = (starting_y_offset * 2f32) / (num_of_buttons - 1) as f32;
 
-        let (width, height) = { 
-            println!("Trying to fetch `ScreenDimensions`.");
-            let dims = world.read_resource::<ScreenDimensions>();
-            (dims.width(), dims.height())
-        };
-        // Get the middle width of the screen.
-        let middle_x = (width / 2f32).round();
-        // Get the middle height of the screen.
-        let middle_y = (height / 2f32).round();
+        let y = starting_y_offset - y_step * idx as f32;
 
-        let x_offset_width = (size_width / 2f32).round();
-        let y_offset_heigth = (size_height / 2f32).round();
-
-        x = middle_x - x_offset_width;
-        y = middle_y - num_of_buttons as f32 * Self::BUTTON_OFFSET + idx as f32 * Self::BUTTON_OFFSET - y_offset_heigth;
-
-        (x, y)
+        y
     }
 
     fn acquire_size(text_len: usize) -> (f32, f32) {
-        let size_width = text_len as f32 * Self::FONT_SIZE;
-        let size_height = Self::FONT_SIZE + 10f32;
+        let size_width = 0.6 * ( text_len as f32 * Self::FONT_SIZE);
+        let size_height = Self::FONT_SIZE;
 
         (size_width, size_height)
     }
+
 }
